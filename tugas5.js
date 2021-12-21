ebe26cbc667d6f048686ebee117cd0f9c9dd3eb9
dimaswntech <67653363+dimaswntech@users.noreply.github.com>
2021-12-21 16:04:15 +0700

    Add files via upload

---
+++ tugas5.js
@@ -0,0 +1,25 @@
+// 1. Pastikan Teman Teman Sudah Menginstall Node JS
+
+// 2. Teman - Teman Buatlah File di Folder javascript teman teman sebuah file dengan nama file tugas5.js
+
+// 3. Buatlah Program Dengan Spesifikasi dibawah ini :
+
+// Buatlah sebuah perulangan dengan kondisi <=20 atau sebanyak 20 kali
+// Setelah itu teman teman tampilkan nilai kelipatan 3 pada perulangan tersebut
+// Tampilkan hasilnya dengan console.log()
+// Hint : Tampilan Output akan berupa nilai 3 , 6 ,9 , 12 ,15 dst.
+
+// 4. Happy Codding :)
+
+for (var i = 1; i<=20; i++){
+
+    if(i%3 === 0){
+      console.log(i)
+    }
+    }
+
+    var j = 3;
+do {
+	console.log(j)
+	j=j+3
+}while(j <=20)
